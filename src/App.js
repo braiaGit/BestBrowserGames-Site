bc0eba7e687f6a350fc3b83fec4f9fd3b4beb898
braiaGit <ryan.ssouzza.s@gmail.com>
2023-11-02 19:47:16 -0300

    Adcionado -Header -Card de bem vindo -Sessão de login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './Header';
-
+import LoginSection from './LoginSection';
+import WelcomeCard from './WelcomeCard';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <WelcomeCard/>
+      <main className='container'>
+      <LoginSection/>
+      
+      </main>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,67 @@
 function Header(){
     return(
-        <header className="p-3 mb-3 border-bottom">
-    <div className="container">
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
-          <h1>BBGames</h1>
-        </a>
-
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" className="nav-link px-2 link-secondary">Overview</a></li>
-          <li><a href="#" className="nav-link px-2 link-body-emphasis">Inventory</a></li>
-          <li><a href="#" className="nav-link px-2 link-body-emphasis">Customers</a></li>
-          <li><a href="#" className="nav-link px-2 link-body-emphasis">Products</a></li>
-        </ul>
-
-        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" className="form-control" placeholder="Search..." aria-label="Search"/>
-        </form>
-
-        <div className="dropdown text-end">
-          <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle"/>
+      <header className="p-3 text-bg-dark">
+      {/* <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <h1>BBGames</h1>
           </a>
-          <ul className="dropdown-menu text-small">
-            <li><a className="dropdown-item" href="#">New project...</a></li>
-            <li><a className="dropdown-item" href="#">Settings</a></li>
-            <li><a className="dropdown-item" href="#">Profile</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Sign out</a></li>
+  
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="#" className="nav-link px-2 text-white">About</a></li>
           </ul>
+  
+          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
+          </form>
+  
+          <div className="text-end">
+            <button type="button" className="btn btn-outline-light me-2">Login</button>
+            <button type="button" className="btn btn-warning">Sign-up</button>
+          </div>
         </div>
+      </div> */}
+
+      <nav class="navbar navbar-expand-md bg-dark sticky-top border-bottom" data-bs-theme="dark">
+    <a class="navbar-brand" href="#">
+    <h1>BBGames</h1>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="#offcanvas" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="#offcanvas" aria-labelledby="#offcanvasLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="#offcanvasLabel">Aperture</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav flex-grow-1 justify-content-between">
+          <li class="nav-item"><a class="nav-link" href="#">
+          </a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Tour</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Product</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Features</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Support</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
+        </ul>
       </div>
     </div>
-  </header>
+
+    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
+    </form>
+
+    <div className="text-end">
+            <button type="button" className="btn btn-outline-light me-2">Login</button>
+            <button type="button" className="btn btn-warning">Sign-up</button>
+          </div>
+  </nav>
+    </header>
+    
     )
 }
 
